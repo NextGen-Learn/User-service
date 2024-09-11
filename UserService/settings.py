c1e6f581c81422dd66c5d41c02ca98e64f231bd3
MatveySviadysh <mkomp06@gmail.com>
2024-09-11 11:09:33 +0300

    chenge db

--- UserService/settings.py
+++ UserService/settings.py
@@ -51,8 +51,12 @@ WSGI_APPLICATION = 'UserService.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'user_db',
+        'USER': 'user_user_db',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
