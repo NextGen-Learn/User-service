2323c47bb2a930271698442de6ceca98932da371
MatveySviadysh <mkomp06@gmail.com>
2024-09-14 21:26:05 +0300

    add model tutor

--- Main/models.py
+++ Main/models.py
@@ -12,3 +12,29 @@ class UserDefault(models.Model):
 
     class Meta:
         db_table = 'UserDefault'
+
+class Tutor(models.Model):
+    id = models.IntegerField(primary_key=True)
+    email = models.EmailField()
+    rating = models.FloatField()
+    user_name = models.CharField(max_length=255)
+    phone_number = models.IntegerField()
+    avatar = models.CharField(max_length=255)
+    bio = models.TextField()
+    education = models.TextField()
+    verified = models.BooleanField()
+    availability = models.JSONField()
+    experience = models.TextField()
+    photo = models.CharField(max_length=255)
+    services = models.CharField(max_length=255)
+    price = models.FloatField()
+    video_id = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Репетитор'
+        verbose_name_plural = 'Репетиторы'
+
+    def __str__(self):
+        return self.user_name
