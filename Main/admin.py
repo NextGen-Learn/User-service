ef06b1cd7f234d6f60f6c8ef90225e206b55fabb
MatveySviadysh <mkomp06@gmail.com>
2024-09-18 20:20:30 +0300

    register mode user to admin panel

--- Main/admin.py
+++ Main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserDefault
 
-# Register your models here.
+class UserDefaultAdmin(admin.ModelAdmin):
+    list_display = ('user_name', 'email', 'phone_number', 'last_login')
+    search_fields = ('user_name', 'email')
+
+admin.site.register(UserDefault, UserDefaultAdmin)
